e599db72e61ac34e8f8f065490b27568a2252c54
yjindata <hf746@naver.com>
2019-01-24 16:27:58 +0900

    Add all my files

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import {Provider } from 'react-redux';
+
+import Posts from './components/Posts';
+import Postform from './components/Postform';
+
+import store from './store';
+class App extends Component {
+  componentWillMount() {
+    fetch('');
+  }
+
+  render() {
+    return (
+      <Provider store={store}>
+        <div className="App">
+          <Postform />
+          <hr />
+          <Posts />
+        </div>
+      </Provider>
+    );
+  }
+}
+
+export default App;
